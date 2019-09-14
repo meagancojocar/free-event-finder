806e7d55d185e1a06cd55fce4dcde82aeef6349e
“meagancojocar” <meagan.cojocar@gmail.com>
2019-09-14 10:34:50 +0800

    doing tings

--- eventFinderApp/models.py
+++ eventFinderApp/models.py
@@ -6,7 +6,14 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     start_time = models.DateTimeField('start time and date')
     end_time = models.DateTimeField('end time and date')
+    venue = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
