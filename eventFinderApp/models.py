9eb75929a1e1410d762afa253b01b51c167111d1
“meagancojocar” <meagan.cojocar@gmail.com>
2019-09-14 11:37:46 +0800

    added many to many relationship

--- eventFinderApp/models.py
+++ eventFinderApp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-
 class Event(models.Model):
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     start_time = models.DateTimeField('start time and date')
     end_time = models.DateTimeField('end time and date')
     venue = models.CharField(max_length=200)
+    categories = models.ManyToManyField('Category', related_name='event')
 
     def __str__(self):
         return self.title
@@ -16,4 +16,4 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.title
+        return self.name
